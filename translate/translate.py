25edfcdacbdbdebe23aed1babb71aa39c0994221
Niharrai <50584781+Niharrai@users.noreply.github.com>
2020-02-22 17:07:09 +0530

    Update translate.py

--- translate/translate.py
+++ translate/translate.py
@@ -35,19 +35,18 @@ def xo(str,binary):
 #def xs(str,binary):
 
 
-def d(str,binary):
-    """Translate D format"""
-    op = binary
-    temp = re.findall(r'\d+', str) 
-    reg = list(map(int, temp)) 
-    reg[0]=reg[0]<<5
-    reg[1]=reg[1]<<15
-    reg[2]=reg[2]<<10
-    print(sum(reg))
-    print(reg[1])
-    b32=format((op + sum(reg)),"032b")
-    print(b32)
-    return b32
+#def d(str,binary):
+#    """Translate D format"""
+#    op = binary
+#    temp = re.findall(r'\d+', str) 
+#    reg = list(map(int, temp)) 
+#    reg[0]=reg[0]<<5
+#    reg[1]=reg[1]<<15
+#    reg[2]=reg[2]<<10
+#    print(sum(reg))
+#    print(reg[1])
+#    b32=format((op + sum(reg)),"032b")
+#    return b32
 
 
 def translate(str,symtab):
@@ -60,4 +59,4 @@ def translate(str,symtab):
     elif(form == 2):
         xo(instr[1])
         
-d("R3,-15(R5)",(31,28,0))
+#d("R3,-15(R5)",(31,28,0))
