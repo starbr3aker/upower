a2a39cfbaed8d9bef468087b6beaa307f5c212c6
Shrvan Warke <38830491+ShadowFox144@users.noreply.github.com>
2020-02-25 07:01:29 +0530

    Added Lables and execute command
    Branch command returns the address of the instruction to go to(Lable 
    Dict - SymTab_Lable)

--- main.py
+++ main.py
@@ -20,6 +20,20 @@ labels = []
 dtype = []
 values = []
 
+SymTab_Lable = {}
+
+class Instruction:
+    def __init__(self, mnemnomic, output, input, address):
+        self.mnemnomic = mnemnomic
+        self.output = output
+        self.input = input
+        self.address = address
+
+    def readAddres(self)
+        return self.address
+
+Instruction_Set = []
+
 
 def initialise():
     """Main memory implementation with dictionary. Isn't faithful recreation of memory because of RAM constraints."""
@@ -61,6 +75,12 @@ def initialise():
     register = {"r{}".format(i): 0 for i in range(32)}
     print("32 registers ready")
 
+def addToSymTab(lable, type, address):
+    if(type): #If its a lable
+        SymTab_Lable[lable] = address
+
+def RedSymTab_Lable(lable): # Returns the memory of the lable
+    return SymTab_Lable[lable]
 
 initialise()
 
@@ -74,9 +94,9 @@ def makesymboltable(path):
     global values
     global symtab
     for i in data:
-        labels.append(i.split()[0].replace(":", ""))
-        dtype.append(i.split()[1].replace(".", ""))
-        values.append(i.split()[2])
+        labels.append(i.split(maxsplit=2)[0].replace(":", ""))
+        dtype.append(i.split(maxsplit=2)[1].replace(".", ""))
+        values.append(i.split(maxsplit=2)[2])
 
     # print(labels)
     # print(dtype)
@@ -84,9 +104,17 @@ def makesymboltable(path):
 
     for i in range(len(labels)):
         if dtype[i] == "word":
-            static[staticend] = values[i]
-            symtab.update({labels[i]: staticend})
-            staticend = staticend + 1
+            if len(values[i].split(",")) > 1:
+                print(values[i])
+                valueslist = values[i].split(",")
+                for j in range(len(valueslist)):
+                    static[staticend] = int(valueslist[j].strip())
+                    print(static[staticend])
+                    staticend = staticend + 1
+            else:
+                static[staticend] = int(values[i])
+                symtab.update({labels[i]: staticend})
+                staticend = staticend + 1
         elif dtype[i] == "asciiz":
             values[i] = values[i].replace('"', "")
             static[staticend] = len(values[i])
@@ -95,9 +123,36 @@ def makesymboltable(path):
                 static[staticend] = values[i][j]
                 staticend = staticend + 1
 
+    for i in range(len(text)):
+        line = text[i]
+        if line[0] != '#' : #Ignore Comments
+            if ":" in line: #Chk for Lable
+                lable = line.split(":",1)
+                lable[1] = lable[1].strip()
+                lable[0] = lable[0].strip()
+                type = not lable[1];
+                addToSymTab(lable[0], type, i)
+            else: #Instructions
+                line = line.replace(","," ")
+                line = line.split(" ")
+                line = [i for i in line if i]
+                #print(line)
+                mnemnomic = line[0]
+                output = line[1]
+                inp = []
+                for i in range (2, len(line)):
+                    inp.append(line[i])
+
+                Instruction_Set.append(Instruction(mnemnomic, output, input, i))
+
     # print(symtab)
+    # for i in range(len(labels)):
+    #     print(static[symtab[labels[i]]])
 
     return symtab
+
+
+# makesymboltable("asm_files/1.s")
 
 
 def storeintoregister(address, value):
@@ -123,13 +178,13 @@ def readfromlabel(label, address):
     return dtype[idx]
 
 
-def exec(line):
+def exec(line, pos):
     instr = line.split()[0]
     global register
     global symtab
     global static
     # print(instr)
-    print(line)
+    # print(line)
     if instr == "add":
         res = line.split()[1].replace(",", "")
         t1 = line.split()[2].replace(",", "")
@@ -137,22 +192,54 @@ def exec(line):
         t1 = register[t1]
         t2 = register[t2]
         register[res] = t1 + t2
-        print("r{} + r{} = {}, at r{}".format(t1, t2, t1 + t2, res))
-    if instr == "ld" or instr == "lhz":
+        print("{} + {} = {}, at {}".format(t1, t2, t1 + t2, res))
+    elif instr == "addi":
+        res = line.split()[1].replace(",", "")
+        t1 = line.split()[2].replace(",", "")
+        t2 = line.split()[3].replace(",", "")
+        t1 = register[t1]
+        t2 = int(t2)
+        register[res] = t1 + t2
+        print("{} + {} = {}, at {}".format(t1, t2, t1 + t2, res))
+    elif instr == "ld" or instr == "lhz":
         line = line.replace(",", "")
         target = line.split()[1]
         print(line)
         loc = line.split()[2]
         disp = loc.split("(")[0]
         loc = loc.split("(")[1].replace(")", "")
-        print("{} {} {}/*".format(target, disp, loc))
-
+        if loc in symtab.keys():
+            register[target] = static[symtab[loc] + int(disp)]
+        else:
+            register[target] = static[register[loc] + int(disp)]
+        print("{} is now {}/*".format(target, register[target]))
+    elif instr == "std":
+        line = line.replace(",", "")
+        source = line.split()[1]
+        print(line)
+        dest = line.split()[2]
+        disp = dest.split("(")[0]
+        dest = dest.split("(")[1].replace(")", "")
+        static[symtab[dest] + int(disp)] = register[target]
+        print(
+            "{} is now {}/*".format(
+                symtab[dest] + int(disp), static[symtab[source] + int(disp)]
+            )
+        )
+    return pos;
 
 def execute(path):
     symtab = makesymboltable(path)  # noqa: F841
     _, text = readasm(path)
-    for line in text:
-        exec(line)
+    global register
+    register["r2"] = 3
+    register["r3"] = 4
 
+    for i in range(len(Instruction_Set)):
+         i = exec(text[Instruction_Set[i].readAddres()], i);
+
+    # for line in text:
+    #
+    #     exec(line)
 
 execute("asm_files/test.s")
